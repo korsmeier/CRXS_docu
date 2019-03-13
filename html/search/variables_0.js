7e89c8cf634c197d04d29a6cb8a2a9717e2ba83a
Michael Korsmeier <korsmeier@physik.rwth-aachen.de>
2019-03-13 17:48:11 +0100

    Updated documentation

--- html/search/variables_0.js
+++ html/search/variables_0.js
@@ -1,6 +1,8 @@
 var searchData=
 [
+  ['f_5ftotxs_5fisread',['f_totXS_IsRead',['../class_c_r_x_s_1_1_x_s__definitions.html#aa6117c7890da8a6bf578b3cc49d3f3ae',1,'CRXS::XS_definitions']]],
   ['fxs_5f_5fel_5fpbarp',['fXS__el_pbarp',['../class_c_r_x_s_1_1_x_s__definitions.html#a8dd4913cb617faf50ec7b1fcbef4cf19',1,'CRXS::XS_definitions']]],
   ['fxs_5f_5fnar_5fpbard',['fXS__nar_pbarD',['../class_c_r_x_s_1_1_x_s__definitions.html#a9896f88f427a85151720b4e97835686e',1,'CRXS::XS_definitions']]],
-  ['fxs_5f_5ftot_5fpbard',['fXS__tot_pbarD',['../class_c_r_x_s_1_1_x_s__definitions.html#a607f477b6b582244626fc99a3c30f86c',1,'CRXS::XS_definitions']]]
+  ['fxs_5f_5ftot_5fpbard',['fXS__tot_pbarD',['../class_c_r_x_s_1_1_x_s__definitions.html#a607f477b6b582244626fc99a3c30f86c',1,'CRXS::XS_definitions']]],
+  ['fxs_5f_5ftot_5fpbarp',['fXS__tot_pbarp',['../class_c_r_x_s_1_1_x_s__definitions.html#a68cd9a2fd2c135c638d790e3f774d1a4',1,'CRXS::XS_definitions']]]
 ];
